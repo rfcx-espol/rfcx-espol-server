ee339717db85ebfff1d08728a83b95bbc51e8c1c
Gustavo <gtotoy@gmail.com>
2018-02-06 14:49:30 -0500

    Deploy script modified

--- RfcxServer/deploy.sh
+++ RfcxServer/deploy.sh
@@ -2,8 +2,8 @@ type ffmpeg # ffmpeg is a dependency
 . ./config.sh
 #APP_DIR=/var/rfcx-espol-server/
 
-rm -r $APP_DIR
 mkdir $APP_DIR
+find $APP_DIR  -type f ! -name 'files' -print0 | xargs -0 rm -vf
 
 # Web Application
 cd ./WebApplication
@@ -19,13 +19,13 @@ systemctl start rfcx-espol-server.service
 # Icecast
 cd ./Icecast
 cp ./refresh_playlists.sh $APP_DIR
-cp -r ./icecast-config $APP_DIR/icecast-config
+cp -r ./icecast-config $APP_DIR/
 chmod -R 777 $APP_DIR/icecast-config
-cp -r ./icecast-web $APP_DIR/icecast-web
+cp -r ./icecast-web $APP_DIR/
 chmod -R 777 $APP_DIR/icecast-web
-cp -r ./icecast-admin $APP_DIR/icecast-admin
+cp -r ./icecast-admin $APP_DIR/
 chmod -R 777 $APP_DIR/icecast-admin
-cp -r ./icecast-log $APP_DIR/icecast-log
+cp -r ./icecast-log $APP_DIR/
 chmod -R 777 $APP_DIR/icecast-log
 cp ./rfcx-espol-playlist.service /etc/systemd/system
 sed -i 's/ICE_USER/'$ICE_USER'/g' /etc/systemd/system/rfcx-espol-playlist.service
