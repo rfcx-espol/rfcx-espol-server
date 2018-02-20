6a6593dd295779be359fdc6d9e849bc586375936
javierron <darkblade_jra@hotmail.com>
2018-02-20 15:35:18 -0500

    Update refresh_playlists.sh

--- RfcxServer/Icecast/refresh_playlists.sh
+++ RfcxServer/Icecast/refresh_playlists.sh
@@ -9,19 +9,21 @@ icecast_command="icecast -b -c /var/rfcx-espol-server/icecast-config/icecast.xml
 
 echo | $icecast_command &
 
-while [ 1 -gt 0 ]; do # while loop, run forever
+# while loop, run forever
+while [ 1 -gt 0 ]; do
 
-	
-	for i in 0 1 2 3 # assuming 4 devices, should be extended to n devices eventually
+	#Asuming 4 devices, should be extended to n devices eventually
+	for i in 0 1 2 3
 	do
-		
-		find $var_path$i$extension -mmin +10 -type f -delete # delete all files older than 10 minutes
-		find $var_path$i$extension -type f -size +20k > $var_path$i/playlist.txt # create playlist with remaining files in folder
-	
+		#Dlete all files older than 10 minutes
+		find $var_path$i$extension -mmin +10 -type f -delete
+		#Create playlist with remaining files in folder
+		find $var_path$i$extension -type f -size +20k > $var_path$i/playlist.txt
 		ices_command="ices /var/rfcx-espol-server/icecast-config/ices-playlist-$i.xml"
-		$ices_command & # call IceS excecutable
+		#Call IceS excecutable
+		$ices_command &
 	done
 	echo ices raise
-	sleep 5m # repeat every 5 minutes
+	#Repeat every 5 minutes
+	sleep 5m
 done
-
