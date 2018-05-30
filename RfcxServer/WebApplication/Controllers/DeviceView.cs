55dfba58f53ea2a8ae43aebfa0c7dc50be2ea274
dajocape <danijo1995@gmail.com>
2018-05-30 07:41:27 -0500

    Changes in device view

--- RfcxServer/WebApplication/Controllers/DeviceView.cs
+++ RfcxServer/WebApplication/Controllers/DeviceView.cs
@@ -1,10 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Text.Encodings.Web;
 
 namespace WebApplication {
 
     public class DeviceViewController : Controller {
-        public IActionResult Index() {
+        public IActionResult Index(string deviceName) {
+            Console.WriteLine("dispositivo "+deviceName);
+            ViewData["deviceName"] =deviceName;
+            
             return View();
         }
     }
