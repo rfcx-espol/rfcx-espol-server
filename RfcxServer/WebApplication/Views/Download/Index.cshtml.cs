1124e261cf0eb44d88deb9fb3ec0efdad8d769a8
dajocape <danijo1995@gmail.com>
2018-07-16 21:42:42 -0500

    Correction of files

---
+++ RfcxServer/WebApplication/Views/Download/Index.cshtml.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication.Views.Download
+{
+    public class DOwnloadModel : PageModel
+    {
+        private List<String> stations;
+
+        public void OnGet()
+        {
+            
+            stations = new List<String>();
+            DirectoryInfo DI = new DirectoryInfo("files");
+            foreach(var item in DI.GetFiles())
+            {
+                stations.Add(item.Name);
+            }
+            
+
+        }
+    }
+}
