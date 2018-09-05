f48c560c3281e7ae53c31498b6e0055659ab4577
Harold <haragon@espol.edu.ec>
2018-09-05 13:30:35 -0500

    change constants

--- RfcxServer/WebApplication/Models/Constants.cs
+++ RfcxServer/WebApplication/Models/Constants.cs
@@ -2,11 +2,12 @@ namespace WebApplication.Models
 {
     public class Constants
     {
+        public const string APP_DIRECTORY="/home/estudiante/rfcx-espol-server/Rfcxserver";
         public const string serverDirecrtory="/var/rfcx-espol-server/";
         public const string BASE_LINK="station0";
         public const string SERVER_ICECAST_CONFIG_DIRECTORY="/var/rfcx-espol-server/icecast-config/";
         public const string TEMPLATE_ICECAST_CONFIG="template/ices-playlist-0.xml";
-        public const string APP_ICECAST_CONFIG_DIRECTORY="../Icecast/icecast-config/";
+        public const string APP_ICECAST_CONFIG_DIRECTORY=APP_DIRECTORY+"/Icecast/icecast-config/";
         public const string TEMPLATE_ICECAST_CONFIG_FILENAME="ices-playlist-0.xml";
         public const string PLAYLIST_FILE_NAME="playlist.txt";
         public const int USER_ID=1000;
