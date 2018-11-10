1fa05a1139fda7f7c7e8be0beefbb3199a7abf65
dajocape <danijo1995@gmail.com>
2018-11-10 18:40:19 -0500

    Correction of kind elimination

--- RfcxServer/WebApplication/Repository/KindRepository.cs
+++ RfcxServer/WebApplication/Repository/KindRepository.cs
@@ -8,6 +8,8 @@ using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 using System;
 using MongoDB.Driver;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace WebApplication.Repository
 {
@@ -96,10 +98,30 @@ namespace WebApplication.Repository
             try
             {
                 DeleteResult actionResult = await _context.Kinds.DeleteOneAsync(
-                        Builders<Kind>.Filter.Eq("Id", KindId));
+                Builders<Kind>.Filter.Eq("Id", KindId));
+                var filter1 = Builders<Photo>.Filter.Eq("KindId", KindId);
+                _context.Photos.DeleteMany(filter1);
+                Core.MakeRecyclerFolder();
+                string kindDeletedPath = Core.KindFolderPath(KindId.ToString());
+                string reclyclerPath = Core.RecyclerFolderPath();
+                string photoName="";
+                string recyclerName="";
+                string[] filesInRecycler = Directory.GetFiles(reclyclerPath);
 
-                return actionResult.IsAcknowledged 
-                    && actionResult.DeletedCount > 0;
+                if (Directory.Exists(kindDeletedPath))
+                {
+                    /*string[] photos = Directory.GetFiles(kindDeletedPath);
+
+                    foreach (string photo in photos)
+                    {
+                        photoName = Path.GetFileName(photo);
+                        recyclerName = Path.Combine(reclyclerPath, photoName);
+                        File.Move(photo, AutoRenameFilename(recyclerName));
+                    }*/
+                    DeleteDirectory(kindDeletedPath);
+
+                }
+                return actionResult.IsAcknowledged && actionResult.DeletedCount > 0;
             }
             catch (Exception ex)
             {
@@ -158,6 +180,54 @@ namespace WebApplication.Repository
             catch (Exception ex)
             {
                 throw ex;
+            }
+        }
+
+        private string AutoRenameFilename(String fileCompleteName)
+        {
+            if (File.Exists(fileCompleteName))
+            {
+                string folder = Path.GetDirectoryName(fileCompleteName);
+                string filename = Path.GetFileNameWithoutExtension(fileCompleteName);
+                string extension = Path.GetExtension(fileCompleteName);
+                int number = 1;
+
+                Match regex = Regex.Match(fileCompleteName, @"(.+) \((\d+)\)\.\w+");
+
+                if (regex.Success)
+                {
+                    filename = regex.Groups[1].Value;
+                    number = int.Parse(regex.Groups[2].Value);
+                }
+
+                do
+                {
+                    number++;
+                    fileCompleteName = Path.Combine(folder, string.Format("{0} ({1}){2}", filename, number, extension));
+                }
+                while (File.Exists(fileCompleteName));
+            }
+            return fileCompleteName;
+        }
+
+        public static void DeleteDirectory(string path)
+        {
+            foreach (string directory in Directory.GetDirectories(path))
+            {
+                DeleteDirectory(directory);
+            }
+
+            try
+            {
+                Directory.Delete(path, true);
+            }
+            catch (IOException) 
+            {
+                Directory.Delete(path, true);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                Directory.Delete(path, true);
             }
         }
 
