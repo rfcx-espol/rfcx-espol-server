c866e7faff5c69a8054b159a452b302b5c06c408
roberto <alexandro.ramr777@gmail.com>
2019-07-31 00:19:47 -0500

    show historical values by sensor

--- RfcxServer/WebApplication/wwwroot/js/charts/byDateRange.js
+++ RfcxServer/WebApplication/wwwroot/js/charts/byDateRange.js
@@ -2,28 +2,12 @@
 let date_pickers = Array.from(document.getElementsByClassName("date-picker"));
 date_pickers.forEach(function(date_picker){
     let now = moment();
-    date_picker.value = now.subtract(7,'days').format('YYYY-MM-DD');
+    date_picker.value = now.subtract(7,'days').format('YYYY-MM-DD');    
 });
-//console.log(date_pickers[0]);
 
 //get Id of Station
 var stationId = $("#stationId").text();
 
-/*** get sensors info, this depends on the html response of ~/ByDateRange ***/
-var sensorDivs = Array.from(document.querySelectorAll("div.sensor"));
-
-var sensors  = sensorDivs.map(function(sensorDiv){
-    
-    let sensorId = sensorDiv.querySelector("div.sensorId").textContent;    
-    let sensorType = sensorDiv.querySelector("div.sensorType").textContent;
-    let startTimestamp = moment(document.querySelector("input.date-picker").value).unix();
-    let initialDataUrl = `api/Station/${stationId}/Sensor/${sensorId}/AvgPerDate?StartTimestamp=${startTimestamp}`;
-    return {
-        sensorId : sensorId,
-        sensorType : sensorType,       
-        initialDataUrl : initialDataUrl
-    }
-});
 
 //In this view, we use only one chart
 //get div id to build a CanvasJs chart
@@ -33,84 +17,14 @@ var canvasJsChartDivId = canvasJsChart.getAttribute("id");
 //create chart
 var chart = avgPerDateChart(canvasJsChartDivId);
 
-var faked_temp_dataPoints = [
-    {x : new Date(2019,6,17), y : 25.09 },    
-    {x : new Date(2019,6,18), y : 26.78 },    
-    {x : new Date(2019,6,19), y : 35.00 },    
-    {x : new Date(2019,6,20), y : 25.68 },    
-    {x : new Date(2019,6,21), y : 26.21 },    
-    {x : new Date(2019,6,22), y : 28.62 },    
-    {x : new Date(2019,6,23), y : 22.34 },    
-    {x : new Date(2019,6,24), y : 24.01 },
-];
-
-var faked_hum_dataPoints = [
-    {x : new Date(2019,6,17) , y : 35.09 },    
-    {x : new Date(2019,6,18) , y : 33.78 },    
-    {x : new Date(2019,6,19) , y : 40.00 },    
-    {x : new Date(2019,6,20) , y : 36.68 },    
-    {x : new Date(2019,6,21) , y : 36.21 },    
-    {x : new Date(2019,6,22) , y : 39.62 },    
-    {x : new Date(2019,6,23) , y : 32.34 },    
-    {x : new Date(2019,6,24) , y : 35.01 },
-];
-
-sensors.forEach(function(sensor){
-    if (sensor.sensorType == "Temperature"){
-
-        //update the chart
-        chart.options.data.push({            
-            legendMarkerType: "circle",
-            toolTipContent: "{y} C°",
-            name : sensor.sensorType,
-            showInLegend: true,
-            xValueType: "dateTime",
-            type : "stackedArea",
-            dataPoints: faked_temp_dataPoints
-        });
-
-        //render changes
-        chart.render();
-
-    } else if (sensor.sensorType == "Humidity") {
-
-        //update the chart
-        chart.options.data.push({            
-            legendMarkerType: "circle",
-            toolTipContent: "{y} %",
-            name : sensor.sensorType,
-            showInLegend: true,
-            xValueType: "dateTime",
-            type : "stackedArea",
-            dataPoints: faked_hum_dataPoints
-        });
-
-        //render changes
-        chart.render(); 
-    } else {
-        //update the chart
-        chart.options.data.push({            
-            legendMarkerType: "circle",
-            toolTipContent: "{y}",
-            name : sensor.sensorType,
-            showInLegend: true,
-            xValueType: "dateTime",
-            type : "stackedArea",
-            dataPoints: faked_hum_dataPoints
-        });
-
-        //render changes
-        chart.render(); 
-    }
-     
-});
-
-/*
-//iterate over sensors to obtain agreggated data
-sensors.forEach(function(sensor){
-    //make request
-    //MUST VALIDATE WHEN THERE IS NO VALUE IN THE LAST K HOURS
-    $.getJSON(sensor.initialDataUrl, function(data){
+let filterButton = document.querySelector("button.button-filter");
+filterButton.addEventListener("click", function(){ 
+    let selectedSensorText = document.querySelector("select.form-control").value;
+    let sensorType = selectedSensorText.trim().split(" ")[0];
+    let sensorId = parseInt(selectedSensorText.trim().split(" ")[2]);
+    let startTimestamp = moment(document.querySelector("input.date-picker").value).unix();
+    let dataUrl = `api/Station/${stationId}/Sensor/${sensorId}/AvgPerDate?StartTimestamp=${startTimestamp}`;
+    $.getJSON(dataUrl, function(data){
         //process response
         let dataPoints = data.map(function(responseElement){            
             let datePart = responseElement[0];
@@ -134,10 +48,11 @@ sensors.forEach(function(sensor){
         })
         .reverse();
         //update the chart
+        chart.options.data.shift();
         chart.options.data.push({            
             legendMarkerType: "circle",
             toolTipContent: "{y} $",
-            name : sensor.sensorType,
+            name : sensorType,
             showInLegend: true,
             xValueType: "dateTime",
             type : "stackedArea",
@@ -147,11 +62,11 @@ sensors.forEach(function(sensor){
         //render changes
         chart.render();   
     });
+    console.log(dataUrl);
 });
-*/
+filterButton.click();
 
 //set behaviour to obtain agreggated data...
-
 
 /*** Aditional webpage behaviour ***/
 setGoToLinkBehaviourInTabs();
