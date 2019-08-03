1062ef8fcd5cb0b4f6bf24a1700e1a65e15e5a3a
roberto <alexandro.ramr777@gmail.com>
2019-08-03 16:05:18 -0500

    add type of chart in historical by date

--- RfcxServer/WebApplication/wwwroot/js/charts/byDate.js
+++ RfcxServer/WebApplication/wwwroot/js/charts/byDate.js
@@ -1,3 +1,5 @@
+$(function(){
+
 //set current date to datepickers
 let date_pickers = Array.from(document.getElementsByClassName("date-picker"));
 date_pickers.forEach(function(date_picker){
@@ -10,46 +12,56 @@ let filterButton = document.querySelector("button.button-filter");
 filterButton.addEventListener("click", function(){ 
     let selectedStationId = document.querySelector("select.form-control").value;
     let startTimestamp = moment(document.querySelector("input.date-picker").value).unix();
-    let dataUrl = `api/Station/${selectedStationId}/AvgPerDate?StartTimestamp=${startTimestamp}`;    
-    //console.log(selectedStationId);
-    $.getJSON(dataUrl, function(data){
-        let sensors = data.map(function(responseElement){
-            let sensorId = responseElement.SensorId;
-            let dataPoints = responseElement.aggregates.map(function(aggregate){
-                let year = aggregate._id.year; 
-                let month =  aggregate._id.month ;
-                let day =  aggregate._id.dayOfMonth;
-                let avg = aggregate.avg;
-                //format x value
-                let x = new Date(year, (month - 1), day);                   
-                //format y value
-                let y = Number(parseFloat(avg).toFixed(2));  
-                console.log(`x:${x} , y:${y}`);
-                return {
-                    "x" : x,
-                    "y" : y 
-                }
+    let dataUrl = `api/Station/${selectedStationId}/AvgPerDate?StartTimestamp=${startTimestamp}`;
+    let sensorsUrl = ` api/Station/${selectedStationId}/Sensor`;    
+    $.getJSON(sensorsUrl,function(sensors){
+        $.getJSON(dataUrl,function(data){      
+            data.forEach(function(responseElement){
+                let sensorId = responseElement.SensorId;                                      
+                let sensorType = sensors.find( s => s.Id == sensorId ).Type;                                  
+                let sensorLocation = sensors.find( s => s.Id == sensorId ).Location;
+                let dataPoints = responseElement.aggregates.map(function(aggregate){
+                    let year = aggregate._id.year; 
+                    let month =  aggregate._id.month ;
+                    let day =  aggregate._id.dayOfMonth;
+                    let avg = aggregate.avg;
+                    //format x value
+                    let x = new Date(year, (month - 1), day);                   
+                    //format y value
+                    let y = Number(parseFloat(avg).toFixed(2));  
+                    //console.log(`x:${x} , y:${y}`);
+                    return {
+                        "x" : x,
+                        "y" : y 
+                    }
+                });            
+                let chartDivId = `chart_${sensorId}`;
+
+                //if there was a previous chart remove it
+                if ( $(`div.historical #${chartDivId}`).length ) {
+                    $(`div.historical #${chartDivId}`).remove();
+                } 
+                let chartDiv = `
+                <div id="${chartDivId}" style="height: 320px" class="canvasJsChart"></div>
+                `;
+                $("div#individual div.historical").append(chartDiv);        
+               
+                //create chart
+                let chart = avgPerDateChart(chartDivId);            
+                let nameOfChart = `${sensorType} ${sensorLocation}`
+                chart.options.data.push({            
+                    legendMarkerType: "circle",
+                    toolTipContent: "{y}",
+                    showInLegend: true,
+                    name : nameOfChart,
+                    xValueType: "dateTime",
+                    type : "area",
+                    dataPoints: dataPoints
+                });
+                //render changes
+                chart.render();      
             });
-            let chartDivId = `chart_${sensorId}`;
-            let chartDiv = `
-            <div id="${chartDivId}" style="height: 320px" class="canvasJsChart"></div>
-            `;
-            $("div#individual div.historical").append(chartDiv);
-            //create chart
-            let chart = avgPerDateChart(chartDivId);
-            chart.options.data.push({            
-                legendMarkerType: "circle",
-                toolTipContent: "{y}",
-                showInLegend: true,
-                xValueType: "dateTime",
-                type : "stackedArea",
-                dataPoints: dataPoints
-            });
-    
-            //render changes
-            chart.render();
-            console.log(dataPoints);
-        });        
+        });
     });
 });
 filterButton.click();
@@ -83,3 +95,5 @@ function avgPerDateChart(divId){
         data:[]
     });
 }
+
+});
