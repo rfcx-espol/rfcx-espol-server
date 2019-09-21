704d97be31f6e86ffaa085d9dcb9f3d47f5a9988
roberto <alexandro.ramr777@gmail.com>
2019-09-21 15:00:39 -0500

    accept name of station as arguments, complete simulate method

--- simulators/station_simulator.py
+++ simulators/station_simulator.py
@@ -1,5 +1,18 @@
 import pymongo
 import time
+import random
+import sys
+import pprint
+
+if len(sys.argv) > 2:
+    print('You have specified too many arguments')
+    sys.exit()
+
+if len(sys.argv) < 2:
+    print('You need to specify the name of the station')
+    sys.exit()
+
+input_station_name = sys.argv[1]
 
 DB_NAME = "db_rfcx00"
 
@@ -11,38 +24,34 @@ station_collection = db["Station"]
 sensor_collection = db["Sensor"]
 data_collection  = db["Data"]
 
-#myquery = { "address": "Park Lane 38" }
-#mydoc = mycol.find(myquery)
-
 """
 to insert documents to Data collection I need :
     {
         "StationId" : 16, //got from Station
         "SensorId" : 1,   //got from Sensor
-        "Timestamp" : NumberLong(1566016729),//generated
+        "Timestamp" : NumberLong(1566016729), //generated
         "Type" : "Temperature", //got from Sensor
         "Value" : 25.11, //generated
         "Units" : "Celcius", //generated
         "Location" : "Environment" //got from Sensor
     }
-    data = {
-        "StationId" : station_id, #got it 
-        "SensorId" : d["Id"], #got it 
-        "Timestamp" : t,
-        "Type" : d["Type"], # got it 
-        "Value" : -25.11, 
-        "Units" : "Celcius", #got it 
-        "Location" : d["Location"] #got it
-    }
 """
 #get id of station using station name
-station_document = station_collection.find_one( { "Name": "Estacion Sansung phone" })
+station_document = station_collection.find_one( { "Name": input_station_name })
+
+if station_document == None:
+    print("There is no such station.")
+    print("These are the stations available:")
+    for s in station_collection.find() :
+        print( '\t"{}"'.format( s['Name'] ) )
+    
+    print('\nNote: Be sure to use quotation marks. ')
+    sys.exit()
+
 station_id = station_document['Id']
-print(station_document['Id'])
 
 #get details of sensors such as Id, Type and Location
 sensor_documents = sensor_collection.find( { "StationId": station_id } )
-t = int(time.time())
 
 class Data(object):
 
@@ -59,35 +68,44 @@ class Data(object):
         elif typeOfSensor == 'Humidity':
             units = 'Percentaje'
         else : 
-            units = 'Unknown'
+            raise Exception('Unknown type of sensor. Please update station_simulator.py, the unknown type of sensor is: {}'.format(typeOfSensor))
         return units
 
+    # returns a dictionary out of the object structure
     def toSimulatedData(self):
-        self.Timestamp  = int(time.time())
-        self.Value = 2
-        print(vars(self))
+        self.Timestamp  = int(time.time()) #set current timestamp in seconds
+        self.Value = self.simulateValue(self.Type)
+        return vars(self) 
     
- 
+    def simulateValue(self, typeOfSensor):    
+        if typeOfSensor == 'Temperature' :
+            value = random.uniform(25,50)
+        elif typeOfSensor == 'Humidity':
+            value = random.uniform(40,60)
+        else : 
+            raise Exception('Unknown type of sensor. Please update station_simulator.py, the unknown type of sensor is: {}'.format(typeOfSensor))
+        return round(value,2)
 
-sensors = {}
-for d in sensor_documents :
-    data = Data(
-        StationId = station_id, 
-        SensorId  = d["Id"],
-        Type      = d["Type"],
-        Location  = d["Location"] 
-    )    
-    #Here I extract the data
-    #so, here I can insert
-    #data_collection.insert_one(data)
-    #print(vars(data))
-    data.toSimulatedData()
-    #print(d)
+#I prepare the abstractions of data to be inserted
+data_abstractions =[]
+for s in sensor_documents:
+        #Here I extract the data
+        data = Data(
+            StationId = station_id, 
+            SensorId  = s["Id"],
+            Type      = s["Type"],
+            Location  = s["Location"] 
+        )
+        data_abstractions.append(data)
 
-
-"""
-cursor = station_collection.find({})
-for document in cursor:
-  station_id = document['Id'] #get id of station
-  print(station_id)
-"""
+#I have the abstractions ready, Is just a matter of call the toSimulatedData() method
+while True:    
+    print("\n --- Simulating station {}... ---".format(input_station_name))
+    for d in data_abstractions:        
+        simulated_data = d.toSimulatedData()
+        #so, here I can insert
+        #data_collection.insert_one(simulated_data)
+        #or print it
+        pprint.pprint(simulated_data)
+        print()
+    time.sleep(5)
