4dff9fab218e5e66e0d5ac8a5fdad8096c8b4adb
AnibalG <aagamboa@espol.edu.ec>
2019-11-13 10:44:53 -0500

    daemonsetup fix

--- raiseAlerts/daemon-setup.sh
+++ raiseAlerts/daemon-setup.sh
@@ -2,8 +2,8 @@
 systemctl stop alert-daemon
 rm /var/alert-daemon/alertRaiser.py
 rm /etc/systemd/system/alert-daemon.service
-rm install_requirements.sh
-rm requirements.txt
+rm /var/alert-daemon/install_requirements.sh
+rm /var/alert-daemon/requirements.txt
 cp requirements.txt /var/alert-daemon
 cp install_requirements.sh /var/alert-daemon
 cp alertRaiser.py /var/alert-daemon
